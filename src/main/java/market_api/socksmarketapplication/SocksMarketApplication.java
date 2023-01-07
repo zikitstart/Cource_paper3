da19d29d0a41ac60848ffae79da932f9685334ac
zikit <zikit.start@yandex.ru>
2023-01-07 12:53:04 +0300

    Первый коммит.

---
+++ src/main/java/market_api/socksmarketapplication/SocksMarketApplication.java
@@ -0,0 +1,13 @@
+package market_api.socksmarketapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksMarketApplication.class, args);
+    }
+
+}
